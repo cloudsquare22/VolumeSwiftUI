4867b722f51388029c72e0793b6d2a4af6afbafb
shi-n <shi-n_xyz@cloudsquare.jp>
2020-05-16 21:06:31 +0900

    Initial Commit

---
+++ VolumeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VolumeSwiftUI
+//
+//  Created by Shin Inaba on 2020/05/16.
+//  Copyright © 2020 shi-n. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
